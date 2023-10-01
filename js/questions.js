71b88726f092ce63ed815dd9d4abe923ff253b48
jataiza <jataiza.barboza@hotmail.com>
2023-10-01 13:37:16 -0300

    Projeto Final

---
+++ js/questions.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    question: "Qual foi a obra em que ela contribuiu?",
+    answers: [
+      { option: "Usina Capivari-Cachoeira", correct: true },
+      { option: "Usina Hidrelétrica de Belo Monte", correct: false },
+      { option: "Usina Hidrelétrica de Itaipu", correct: false },
+    ],
+  },
+  {
+    question: "Qual curso ela fazia?",
+    answers: [
+      { option: "Engenharia da Computação", correct: false },
+      { option: "Engenharia Civil", correct: true },
+      { option: "Engenharia Elétrica", correct: false },
+    ],
+  },
+  {
+    question: "Quantos irmãos ela tinha?",
+    answers: [
+      { option: "8", correct: false },
+      { option: "9", correct: false },
+      { option: "10", correct: true },
+    ],
+  },
+  {
+    question: "Em que tipo de trabalho Enedina se envolveu para pagar seus estudos?",
+    answers: [
+      { option: "Empregada doméstica e Camareira,", correct: false },
+      { option: "Empregada doméstica e Cozinheira", correct: false },
+      { option: "Empregada doméstica e babá", correct: true },
+    ],
+  },
+];
